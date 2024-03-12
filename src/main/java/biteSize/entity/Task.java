dbd97786a68877beff36afb5ca4ef06580918897
Ian DeLoach <iandeloach398@gmail.com>
2024-03-12 18:03:11 -0500

    Add task getter and setters

--- src/main/java/biteSize/entity/Task.java
+++ src/main/java/biteSize/entity/Task.java
@@ -16,4 +16,99 @@ public class Task {
     private User user;
     private int id;
 
+    /**
+     * Getter for name
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Setter for name
+     * @param name the new name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Getter for the deadline
+     * @return deadline
+     */
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    /**
+     * Setter for deadline
+     * @param deadline the new deadline
+     */
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    /**
+     * Getter for the urgency
+     * @return the urgency
+     */
+    public String getUrgency() {
+        return urgency;
+    }
+
+    /**
+     * Setter for urgency
+     * @param urgency the new urgency
+     */
+    public void setUrgency(String urgency) {
+        this.urgency = urgency;
+    }
+
+    /**
+     * Getter for theme
+     * @return the theme
+     */
+    public String getTheme() {
+        return theme;
+    }
+
+    /**
+     * Setter for theme
+     * @param theme
+     */
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    /**
+     * Getter for user
+     * @return the user object associated to the task
+     */
+    public User getUser() {
+        return user;
+    }
+
+    /**
+     * Setter for user
+     * @param user the new user
+     */
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    /**
+     * Getter for ID
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Setter for ID
+     * @param id the new id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
 }
