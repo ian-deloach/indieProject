4c4d3ce0f6e46c28fdb5224b9e48ea9814125b94
Ian DeLoach <iandeloach398@gmail.com>
2024-05-17 18:31:17 -0500

    Add UserController class

---
+++ src/main/java/biteSize/controller/UserController.java
@@ -0,0 +1,11 @@
+package biteSize.controller;
+
+
+/*
+    A class to modify or retrieve the user's information
+    via the use of DAOs
+    @author IanDeLoach
+ */
+public class UserController {
+    //TODO Create method to get the user's information from their email
+}
