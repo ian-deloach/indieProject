57bc4727ecd202d69979caae5c8fd1c6e5bc844e
Ian DeLoach <iandeloach398@gmail.com>
2024-10-28 18:19:53 -0500

    Add ScheduleTest class

---
+++ src/test/java/biteSize/persistence/ScheduleTest.java
@@ -0,0 +1,18 @@
+package biteSize.persistence;
+
+import biteSize.entity.Task;
+import biteSize.entity.Theme;
+import biteSize.entity.User;
+import junit.framework.TestCase;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+import junit.framework.TestCase;
+
+public class ScheduleTest extends TestCase {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+
+}
