72d34ad031e460d44409838be4da669cd73d90f9
ian-deloach <iandeloach398@gmail.com>
2024-11-18 20:08:23 -0600

    Add auth0 boilerplate code

--- src/main/java/biteSize/controller/LogOut.java
+++ src/main/java/biteSize/controller/LogOut.java
@@ -13,23 +13,52 @@ import java.io.IOException;
 
 @WebServlet("/log-out")
 public class LogOut extends HttpServlet {
-    //TODO Add a way to get confirmation the user wants to log out
 
-    /**
-     * Sets the amazon cognito attributes to null
-     * @param req the request
-     * @param resp the response
-     * @throws ServletException
-     * @throws IOException
-     */
+    private String domain;
+    private String clientId;
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        HttpSession session = req.getSession();
-        session.setAttribute("userName", null);
-        session.setAttribute("userEmail", null);
-
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/index.jsp");
-        dispatcher.forward(req, resp);
+    public void init(ServletConfig config) {
+        domain = config.getServletContext().getInitParameter("com.auth0.domain");
+        clientId = config.getServletContext().getInitParameter("com.auth0.clientId");
     }
+
+    @Override
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession() != null) {
+            request.getSession().invalidate();
+        }
+        String returnUrl = String.format("%s://%s", request.getScheme(), request.getServerName());
+        if ((request.getScheme().equals("http") && request.getServerPort() != 80) || (request.getScheme().equals("https") && request.getServerPort() != 443)) {
+            returnUrl += ":" + request.getServerPort();
+        }
+        returnUrl += "/login";
+        String logoutUrl = String.format(
+                "https://%s/v2/logout?client_id=%s&returnTo=%s",
+                domain,
+                clientId,
+                returnUrl
+        );
+        response.sendRedirect(logoutUrl);
+    }
+
+//    //TODO Add a way to get confirmation the user wants to log out
+//
+//    /**
+//     * Sets the amazon cognito attributes to null
+//     * @param req the request
+//     * @param resp the response
+//     * @throws ServletException
+//     * @throws IOException
+//     */
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//
+//        HttpSession session = req.getSession();
+//        session.setAttribute("userName", null);
+//        session.setAttribute("userEmail", null);
+//
+//        RequestDispatcher dispatcher = req.getRequestDispatcher("/index.jsp");
+//        dispatcher.forward(req, resp);
+//    }
 }
